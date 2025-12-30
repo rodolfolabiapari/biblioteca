5689e1a25f0496b7624b402370e1d02a160ae8a1
Rodolfo Labiapari Mansur, MSc. @rodolfolabiapari <rodolfolabiapari@gmail.com>
2025-12-30 20:29:53 -0300

    fix lint

--- export-from-calibry.sh
+++ export-from-calibry.sh
@@ -10,10 +10,11 @@ flatpak run --command=calibredb com.calibre_ebook.calibre list --fields all
 # Exportando o json
 flatpak run --command=calibredb com.calibre_ebook.calibre list \
   --for-machine \
+  --sort-by "*cdd" \
   --fields "title,authors,publisher,tags,isbn,*cdd,*cdd_class,*read" > biblioteca.json
 
 # Exportando csv
-
 flatpak run --command=calibredb com.calibre_ebook.calibre list \
   --fields "title,authors,publisher,tags,isbn,*cdd,*cdd_class" \
-  --separator ',' > csv
+  --sort-by "*cdd" \
+  --separator ';' > biblioteca.csv
